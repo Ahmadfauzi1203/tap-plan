8b29175f43a45bfbee937a33a160bb5310846d50
Ahmadfauzi1203 <ahmadfauzi12346@gmail.com>
2024-01-10 08:01:56 +0700

    commit first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/tap-agri-plan.github.io/",
+  base: "/tap-plan/",
   plugins: [react()],
 });
